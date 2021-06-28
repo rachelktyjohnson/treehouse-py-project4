1e4caac6d2a81f4656569bc05f6c4806489c2bdb
Rachel Johnson <rachelktyjohnson@gmail.com>
2021-06-28 23:57:48 +1000

    Product quantity to integer

--- models.py
+++ models.py
@@ -13,7 +13,7 @@ class Product(Base):
 
     product_id = Column(Integer, primary_key=True)
     product_name = Column('Name', String)
-    product_quantity = Column('Quantity', String)
+    product_quantity = Column('Quantity', Integer)
     product_price = Column('Price', Integer)
     date_updated = Column('Date', Date)
 
